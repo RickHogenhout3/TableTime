d71626feb53135b1d7a30d1ed142cc039b1abee2
MartijnKerpentier <162339@student.horizoncollege.nl>
2025-02-08 15:51:22 +0100

    .env extension

--- config.php
+++ config.php
@@ -1,14 +1,20 @@
 <?php
+require __DIR__ . '/vendor/autoload.php';
+
+$env = Dotenv\Dotenv::createUnsafeImmutable(__DIR__);
+$env->load();
+
 session_start();
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "tabletime";
+$host = getenv('DB_HOST');
+$dbname = getenv('DB_NAME');
+$username = getenv('DB_USER');
+$password = getenv('DB_PASSWORD');
 $charset = "utf8mb4";
 
 try {
-    $connect = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $connect = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connection succeeded!";
 } catch (PDOException $error) {
     $message = $error->getMessage();
     echo "Connection failed: $message";
